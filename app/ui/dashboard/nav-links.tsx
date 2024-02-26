f20fdcc86b1a81afacaf7c9463b3824d5d77543b
firat <firatwells@icloud.com>
2024-02-26 22:48:13 +0300

    tryna fix react

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -21,6 +22,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+ 
   return (
     <>
       {links.map((link) => {
@@ -29,9 +31,10 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600' : pathname === link.href,
+                'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
           >
